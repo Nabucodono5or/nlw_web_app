63c21ac224ffb6ebaa2c5a8766bacb8b180b9308
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-04 07:49:54 -0300

    inserido styles globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import "./assets/styles/global.css";
 // JSX: xml/html dentro do javascript
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <button>Click em mim</button>
     </div>
   );
 }
