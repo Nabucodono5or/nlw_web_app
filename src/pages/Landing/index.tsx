756fdc49f055407d22a404200ae5164d65fbe439
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-07 11:53:10 -0300

    Realizada a conexão com o back end para listar connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import landingImg from "../../assets/images/landing.svg";
 import logoImg from "../../assets/images/logo.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import pupperHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import api from "../../services/api";
 import "./style.css";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      setTotalConnections(response.data.total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -35,7 +44,7 @@ function Landing() {
 
         <div className="total-conexoes">
           <span>
-            Total de 200 conexões realizadas
+            Total de {totalConnections} conexões realizadas
             <img src={pupperHeartIcon} alt="coração roxo" />
           </span>
         </div>
